ed900aa904715d7edd8835de8e3f8980b7483783
Lion77777 <leon.avetisyan.1993@mail.ru>
2025-03-09 18:21:47 +0400

    Create task deleting functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import './App.css'
 import {TodolistItem} from './TodolistItem'
 
@@ -8,18 +9,26 @@ export type Task = {
 }
 
 export const App = () => {
-  const tasks: Task[] = [
+  let [tasks, setTasks] = useState<Task[]>([
     { id: 1, title: 'HTML&CSS', isDone: true },
     { id: 2, title: 'JS', isDone: true },
     { id: 3, title: 'ReactJS', isDone: false },
     { id: 4, title: 'Redux', isDone: false },
     { id: 5, title: 'Typescript', isDone: false },
     { id: 6, title: 'RTK query', isDone: false },
-  ]
+  ]);
+
+  const deleteTask = (taskId: number) => {
+    const filteredTasks = tasks.filter(task => {
+      return task.id !== taskId;
+    });
+
+    setTasks(filteredTasks);
+  }
 
   return (
       <div className="app">
-        <TodolistItem title="What to learn" tasks={tasks} />
+        <TodolistItem title="What to learn" tasks={tasks} deleteTask={deleteTask}/>
       </div>
   )
 }
